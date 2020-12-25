858f70e4a736c4a2ff07b2c604c527d44a46fa80
luml <lumeiling@bonc.com.cn>
2020-12-25 16:57:32 +0800

    first encounter with Vite

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <ul>
+    <li>Get a svg pattern from codepen</li>
+    <li>Make a purple theme with only a home page</li>
+    <li>Make sure it will be continued</li>
+  </ul>
+  <h3>Vite means fast in french, if you don't know, now you know</h3>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style lang="">
+  ul {
+    list-style: none;
+  }
+</style>
