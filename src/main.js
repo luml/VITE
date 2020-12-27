4b17e575e49f2668cbf92c11f00bb056318d317e
luml <lumeiling@bonc.com.cn>
2020-12-27 12:25:21 +0800

    feat: 404 page

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import App from './App.vue'
 import FirstDay from './components/FirstDay.vue'
 import ChildPage from './components/ChildPage.vue'
 import User from './components/User.vue'
+import NotFound from './components/NotFound.vue'
 import './index.css'
 
 
 const routes = [
     { path: '/', component: FirstDay },
     { path: '/child', component: ChildPage },
-    { path: '/users/:id', component: User }
+    { path: '/users/:id', component: User },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ]
 const history = createWebHashHistory()
 const router = createRouter({
