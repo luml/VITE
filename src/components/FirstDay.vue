4d4d2658bcf27fc3a653f443c6187ebf1bc68a2a
luml <lumeiling@bonc.com.cn>
2020-12-31 15:47:40 +0800

    upd: add source links

--- src/components/FirstDay.vue
+++ src/components/FirstDay.vue
@@ -3,6 +3,13 @@
     <p>
       Hey, this is your first day with Vite, hope you have fun at the rest of the time.
     </p>
+    <a target="_blank" href="https://github.com/vitejs/vite">Check Vite on github</a>
+    <br />
+    <a
+      target="_blank"
+      href="https://docs.google.com/presentation/d/1X1hrFw18v67bEniTPpaI_DBulLdkKNFEc_3nVEm95mM/edit#slide=id.ga7017d88ae_0_8"
+      >A material link</a
+    >
   </div>
 </template>
 
